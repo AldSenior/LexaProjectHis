8c1da4370231e3d7ddcb07bcc0062801867c21d9
AldSenior <aldsio196@gmail.com>
2025-05-15 23:31:02 +0300

    a

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,6 @@
 import HomeClient from "./components/HomeClient";
 export const revalidate = 3600;
 
-export const metadata = {
-  title: "Афганская Война",
-  description: ".",
-  keywords: "Афганская Война",
-};
-
 export default function Home() {
   return <HomeClient />;
 }
