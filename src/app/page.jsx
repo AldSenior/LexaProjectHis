b9b2edf99d9e1e21418454c505a654f90699d081
AldSenior <aldsio196@gmail.com>
2025-05-15 23:20:45 +0300

    a

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import HomeClient from "./components/HomeClient";
 export const revalidate = 3600;
 
 export const metadata = {
-  title: "Революция 1917 года",
+  title: "Афганская Война",
   description:
     "Исследуйте события, личности и документы Российской революции 1917 года через интерактивные карты, викторины и хронологии.",
   keywords:
